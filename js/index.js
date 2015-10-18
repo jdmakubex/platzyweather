08b039aa5eab045b5697a7d2a834df444dadde4c
Juan de Dios Marcos <marg.jd@gmail.com>
2015-10-18 15:31:31 -0500

    Paso de  valores del objeto que contiene información extraida de la API a variables de javaScrip para manipularlos

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 (function() {
  var API_WEATHER_KEY = "979dd242d0f5be6fb199133ffe1bc92f";
  var API_WEATHER_URL ="http://api.openweathermap.org/data/2.5/weather?APPID=" + API_WEATHER_KEY + "&" ;
+ var IMG_WEATHER = "http://openweathermap.org/img/w/";
 
  var cityWeather = {};
  cityWeather.zone;
@@ -36,7 +37,13 @@
   };
 
   function getCurrentWeather(data){
-   console.log(data);
+   //console.log(data);
+   cityWeather.zone = datal.name;
+   cityWeather.icon = IMG_WEATHER + data.weather[0].icon + ".png";
+   cityWeather.temp = data.main.temp-273.15;
+   cityWeather.temp_max = data.main.tempo_max - 273.15;
+   cityWeather.temp_min =  data.main.temp_min - 273.15;
+   cityWeather.main= data.weather[0].main;
   }
 
 })();

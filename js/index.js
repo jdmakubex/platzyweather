45177c2ab30bcee002c0397a9e1304ab2861b95a
Juan de Dios Marcos <marg.jd@gmail.com>
2015-10-18 15:01:37 -0500

    Agregando configuracion de Geolocalización, la impresión de la variable  de index.js data me indica que todo se conecta bien

--- js/index.js
+++ js/index.js
@@ -1,4 +1,15 @@
 (function() {
+ var API_WEATHER_KEY = "979dd242d0f5be6fb199133ffe1bc92f";
+ var API_WEATHER_URL ="http://api.openweathermap.org/data/2.5/weather?APPID=" + API_WEATHER_KEY + "&" ;
+
+ var cityWeather = {};
+ cityWeather.zone;
+ cityWeather.icon;
+ cityWeather.temp;
+ cityWeather.temp_max;
+ cityWeather.temp_min;
+ cityWeather.main;
+
 
   if(navigator.geolocation) {
 
@@ -21,6 +32,11 @@
     var lat = position.coords.latitude;
     var lon = position.coords.longitude;
     console.log("Tu posición es: " + lat + "," + lon);
+    $.getJSON(API_WEATHER_URL + "lat=" + lat + "&lon="+lon, getCurrentWeather);
+  };
+
+  function getCurrentWeather(data){
+   console.log(data);
   }
 
 })();

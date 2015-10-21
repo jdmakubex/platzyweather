45f6f485d4cedf0aa96f80d5fba089b89fbc63f7
Juan de Dios Marcos <marg.jd@gmail.com>
2015-10-21 01:02:41 -0500

    Agregando Funcionalidad de la caja de texto y botón

--- js/index.js
+++ js/index.js
@@ -7,6 +7,11 @@
  var today = new Date();
  var timeNow = today.toLocaleTimeString();
 
+ var $body = $("body");
+ var $loader = $("loader");
+ var nombreNuevaCiudad = $("[data-input= 'cityAdd']");
+ var buttonAdd = $("[data-buton='add']");
+
  var cityWeather = {};
  cityWeather.zone;
  cityWeather.icon;
@@ -15,6 +20,15 @@
  cityWeather.temp_min;
  cityWeather.main;
 
+ $( buttonAdd ).on("click", addNewCity);
+
+ $( nombreNuevaCiudad).on("keypress", function(event){
+   //Probando el numero de la tecla Enter, resultó ser 13
+   //console.log(event.which);
+   if(event.which==13){
+    addNewCity(); 
+   }
+ });
 
   if(navigator.geolocation) {
 
@@ -49,7 +63,7 @@
    cityWeather.temp_min = data.main.temp_min - 273.15;
    cityWeather.main= data.weather[0].main;
 
-   renderTemplate();
+   renderTemplate(cityWeather);
  };
 
   function activateTemplate(id){
@@ -57,7 +71,7 @@
     return document.importNode(t.content, true);
   };
 
-  function renderTemplate() {
+  function renderTemplate(cityWeather) {
     var clone = activateTemplate("#template--city");
 
     clone.querySelector("[data-time]").innerHTML = timeNow ;
@@ -66,8 +80,26 @@
     clone.querySelector("[data-temp='max']").innerHTML = cityWeather.temp_max.toFixed(1);
     clone.querySelector("[data-temp='min']").innerHTML = cityWeather.temp_min.toFixed(1);
     clone.querySelector("[data-temp='current']").innerHTML = cityWeather.temp.toFixed(1);
-    $(".loader").hide();
-    $("body").append(clone);
+    $( $loader).hide();
+    $( $body ).append(clone);
+  };
+
+  function addNewCity(event){
+    event.preventDefault();
+    $.getJSON(API_WEATHER_URL + "q=" + $( nombreNuevaCiudad ).val(), getWeatherNewCity);
+  };
+
+  function getWeatherNewCity(data){
+  //  console.log(data);
+  var cityWeather = {};
+  cityWeather.zone = data.name;
+  cityWeather.icon = IMG_WEATHER + data.weather[0].icon + ".png";
+  cityWeather.temp = data.main.temp - 273.15;
+  cityWeather.temp_max = data.main.temp_max - 273.15;
+  cityWeather.temp_min = data.main.temp_min - 273.15;
+  cityWeather.main= data.weather[0].main;
+
+  renderTemplate(cityWeather);
   };
 
 })();
